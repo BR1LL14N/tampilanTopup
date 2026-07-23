625cb8b544374db5bd528aa055cffa49e3581098
itzluthfi <luthfishidqi2@gmail.com>
2026-07-23 10:52:10 +0700

    style: update hero slide 1 banner to /assets/hero/banner1.png

--- components/home/home-content.tsx
+++ components/home/home-content.tsx
@@ -32,7 +32,7 @@ const slides = [
     tag: "Top Up Game Instant",
     title: "TOP UP MLBB & FF TERMURAH",
     desc: "Topup diamond, voucher, dan promo game instan otomatis 24 jam dengan harga termurah.",
-    bg: gameAssets["free-fire"].banner,
+    bg: "/assets/hero/banner1.png",
     btn1: "Topup Sekarang",
     btn2: "Cek Pesanan",
   },
